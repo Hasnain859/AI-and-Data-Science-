0bbbb60686cf9c61074af7cf90285a7987f9ac3e
Hasnain859 <mohammadhasnain372@gmail.com>
2025-08-19 14:28:58 +0500

    Uploading your folder

--- 5.py
+++
@@ -1,6 +0,0 @@
-total = 0
-
-for num in range(1, 51):
-    total += num
-
-print("The sum of numbers from 1 to 50 is:", total)
--- 6.py
+++
@@ -1,5 +0,0 @@
-text = input("Enter a string: ")
-
-print("Characters in the string:")
-for char in text:
-    print(char)
